28d2ebb78797f4b81eb9bf97600357f751745fab
aakash2602-xi <aakashk2602@gmail.com>
2026-09-10 00:43:53 +0530

    Completed : A Ship That Fires Bullets

--- game_functions.py
+++ game_functions.py
@@ -9,8 +9,7 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        new_bullet = Bullet(ai_settings, screen, ship)
-        bullets.add(new_bullet)
+        fire_bullet(ai_settings,screen,ship,bullets)
 
 def check_keyup_events(event, ship):
     """Respond to key-removes."""
@@ -25,9 +24,9 @@ def check_events(ai_settings, screen, ship, bullets):
         if(event.type==pygame.QUIT):
             sys.exit()
         elif(event.type==pygame.KEYDOWN):
-            check_keydown_events(ai_settings, screen, ship, bullets)
+            check_keydown_events(event,ai_settings, screen, ship, bullets)
         elif event.type==pygame.KEYUP:
-            check_keyup_events(ai_settings, screen, ship, bullets)
+            check_keyup_events(event, ship)
 
 def update_screen(ai_settings,screen,ship,bullets):
         screen.fill(ai_settings.bg_color)
@@ -37,3 +36,15 @@ def update_screen(ai_settings,screen,ship,bullets):
             bullet.draw_bullet()
         ship.blitme()
         pygame.display.flip()
+
+def fire_bullet(ai_settings,screen,ship,bullets):
+    if len(bullets) <= ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
+
+def update_bullets(bullets):
+    bullets.update()
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+    print(len(bullets))
