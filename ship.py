7edb3f1d7061a196906abf5a11212326d960744c
aakash2602-xi <aakashk2602@gmail.com>
2026-09-09 23:59:07 +0530

    Added Bullets To Game - Upto Deleting Bullets After Crossing Screen Top

--- ship.py
+++ ship.py
@@ -1,25 +1,33 @@
 import pygame
 
 class Ship():
-    def __init__(self,screen):
+    def __init__(self,ai_settings,screen):
         self.screen=screen
+        self.ai_settings=ai_settings
 
-        # Load the ship image and get its rect.
+        #Load the ship image and get its rect.
         self.image=pygame.image.load('images/ship.bmp')
         self.rect=self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
-        # Start each new ship at the bottom center of the screen.
+        #Start each new ship at the bottom center of the screen.
         self.rect.centerx=self.screen_rect.centerx
         self.rect.bottom=self.screen_rect.bottom
-            #movement flags
+
+        #Store a decimal value for the ship's center.
+        self.center = float(self.rect.centerx)
+
+        #Movement Flags
         self.moving_right=False
         self.moving_left=False
     def update(self):
-        if self.moving_right:
-            self.rect.centerx+=1
-        if self.moving_left:
-            self.rect.centerx-=1
+        if self.moving_right and self.rect.right<self.screen_rect.right:
+            self.center+=self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left>0:
+            self.center-=self.ai_settings.ship_speed_factor
+
+        #Update rect object from self.center.
+        self.rect.centerx = self.center
 
     def blitme(self):
         """Draw the ship at its current location."""
